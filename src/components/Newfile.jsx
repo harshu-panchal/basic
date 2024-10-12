9b906e6c9afafec376135f88bf0f8966d252b699
harshu-panchal <harshvardhanpanc145@gmail.com>
2024-10-12 17:46:34 +0530

    sec commit

--- src/components/Newfile.jsx
+++ src/components/Newfile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Newfile.css'
+import snow from './assets/snowflake.svg'
 
 const Newfile = () => {
   return (
     <div>
       <h1>This is a test file</h1>
-      <img src=".\assets\snowflake.svg" alt="" />
+      <img src={snow} alt="" />
 
       <NavLink to="/basic/file2"><span className='logo-text' >Harshvardhan Panchal</span></NavLink>
 
